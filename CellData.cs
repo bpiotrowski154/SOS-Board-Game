5c2034cad64b6b44d9f0fe4f94f0f5835ad2620b
bpiotrowski154 <88128424+bpiotrowski154@users.noreply.github.com>
2022-10-21 12:03:14 -0500

    Created Cell Data struct and Position classes
    Created a struct called CellData that contains the value to be displayed on the cell and the color of the player. This will be used to check who gets the point when an SOS is formed in the future. The position class basically stores the x and y values of a cell and is used to assign cell data to a position on the board.

---
+++ CellData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOS
+{
+    public struct CellData
+    {
+        public CellData(string value, string colorOfPlayer)
+        {
+            this.value = value;
+            this.colorOfPlayer = colorOfPlayer;
+        }
+        public string value { get; set; }
+        public string colorOfPlayer { get; set; }
+
+    }
+}
