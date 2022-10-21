80e268dcfd490a6ea100730a29a0ab2e080790c6
bpiotrowski154 <88128424+bpiotrowski154@users.noreply.github.com>
2022-10-21 12:36:16 -0500

    Unit Test Project
    Created a new project to implement unit testing

---
+++ GameLogicTests/UnitTests.cs
@@ -0,0 +1,13 @@
+using SOS;
+namespace GameLogicTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
